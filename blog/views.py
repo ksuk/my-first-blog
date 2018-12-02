e4984240ad769362c74a9ac1234674f5c6d516e4
ksuk <keisukeniko7@gmail.com>
2018-12-02 18:04:52 +0900

    soso

--- blog/views.py
+++ blog/views.py
@@ -14,3 +14,32 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, "blog/post_detail.html", {"post": post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "Post":
+        form = PostForm(request.POST, intance=post)
+        if form.id_valid():
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect("post_detail", pk=post.pk)
+        else:
+            form = PostForm(isinstance=post)
+
+        return render(request, "blog/post_edit.html", {"form": form})
